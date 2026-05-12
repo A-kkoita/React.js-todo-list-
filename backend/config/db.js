06de293bf2f44709c0e22ab3c92bbebe46e69e90
unknown <aishakhaitoukoita>
2026-05-12 09:24:10 +0200

    Added the backend with MongoDB

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export const connectDB = async () => {
-  await mongoose.connect('mongodb://localhost:27017/task-manager');
+  await mongoose.connect('mongodb+srv://aishakhaitoukoita_db_user:XjXL0njpdUayI6Ep@cluster0.5ywiv4i.mongodb.net/task-manager');
 
   console.log('MongoDB connected');
 };
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-
+import session from 'express-session';
+import MongoStore from 'connect-mongo'
 import taskrouter from './routes/tasks.js';
 import { connectDB } from './config/db.js';
 
@@ -10,7 +11,17 @@ const app = express();
 const PORT = 4000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin:' http://localhost:5173', credentials: true}));
+
+app.use(
+  session({
+    secret: 'task-manager-secret',
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: 'mongodb+srv://aishakhaitoukoita_db_user:XjXL0njpdUayI6Ep@cluster0.5ywiv4i.mongodb.net/task-manager'}),
+    cookie:{httpOnly: true, maxAge: 1000 * 60 * 60 *24}
+  })
+)
 
 app.use('/api/tasks', taskrouter);
 
